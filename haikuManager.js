b5c3fe82838c44dd9d986e75f497a91189282d1e
LLMinate <ryan.hansen@xy.company>
2024-07-11 00:57:08 -0700

    Update Haiku Image to Background

--- haikuManager.js
+++ haikuManager.js
@@ -38,7 +38,10 @@ function updateBackground() {
 
 function updateHaikuImage() {
   const haikuImage = haikuImages[currentIndex];
-  document.getElementById('haikuImage').src = haikuImage;
+  document.body.style.backgroundImage = 'url(' + haikuImage + ')';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundPosition = 'center';
+  document.body.style.transition = 'background-image 0.5s ease-in-out';
 }
 
 function startRotation() {
