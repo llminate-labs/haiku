39dc98731a35818a14da49bb6ea3f8e2832094a7
LLMinate <ryan.hansen@xy.company>
2024-07-11 00:32:53 -0700

    Add Haiku Images to Homepage

--- haikuManager.js
+++ haikuManager.js
@@ -7,15 +7,6 @@ const haikus = [
   'Autumn leaves drift by,<br>crimson, gold, a fiery dance,<br>cool breeze whispers tales.'
 ];
 
-const haikuImages = [
-  'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-iQzh3ik4BvqdNx9c7lZpC8DP.png',
-  'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-MlDW0hl8wdHLdleHgDwTrZeS.png',
-  'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-2baTgqG8RIGGZ3GBu0HDI6EO.png',
-  'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-4d0Q87nowhlOW5trrfya9Om9.png',
-  'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-fyQcP9NZ3UFFg8nDkYhN6pFn.png',
-  'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-lmw9Hdn9eEsMdo4GTZwMetj6.png'
-];
-
 let currentIndex = 0;
 let autoRotate = true;
 let haikuInterval;
@@ -24,9 +15,9 @@ function updateHaiku() {
   document.getElementById('haiku').style.opacity = 0;
   setTimeout(function() {
     document.getElementById('haiku').innerHTML = haikus[currentIndex].replace(/'/g, "'");
-    document.getElementById('haikuImage').src = haikuImages[currentIndex];
     document.getElementById('haiku').style.opacity = 1;
     updateBackground();
+    updateHaikuImage();
   }, 500);
 }
 
@@ -34,6 +25,19 @@ function updateBackground() {
   const themeMap = { 0: 'spring', 1: 'winter', 2: 'morning', 3: 'ocean', 4: 'mountain', 5: 'autumn' };
   const currentTheme = themeMap[currentIndex];
   document.documentElement.setAttribute('data-theme', currentTheme);
+}
+
+function updateHaikuImage() {
+  const imageMap = {
+    0: 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-xoveDohUTeWTwd7uYScv2uyz/user-7E63qmNDGeNd8DOac52nI0qL/img-ap0YwPT7XJJhih1oMT6Q92jA.png?st=2024-07-11T06%3A32%3A03Z&se=2024-07-11T08%3A32%3A03Z&sp=r&sv=2023-11-03&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-07-10T22%3A32%3A14Z&ske=2024-07-11T22%3A32%3A14Z&sks=b&skv=2023-11-03&sig=a6J%2BvVmyJHnhw9zJt%2BsXshfjmb83VsYvjaHCrMX3TGQ%3D',
+    1: 'https://example.com/winter.jpg',
+    2: 'https://example.com/morning.jpg',
+    3: 'https://example.com/ocean.jpg',
+    4: 'https://example.com/mountain.jpg',
+    5: 'https://example.com/autumn.jpg'
+  };
+  const currentImage = imageMap[currentIndex];
+  document.getElementById('haikuImage').src = currentImage;
 }
 
 function startRotation() {
